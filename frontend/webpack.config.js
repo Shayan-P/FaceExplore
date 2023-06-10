461ea78b8b2fb568ba21bdafb683f22b826d61a6
Shayan-P <shayanpardis82@gmail.com>
2023-06-10 15:18:58 -0400

    add webpack

---
+++ frontend/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: './src/index.html', to: 'index.html' },
+                { from: 'src/style.css', to: 'style.css' }
+            ],
+        }),
+    ],
+};
