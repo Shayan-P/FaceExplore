ee6daab777265226f5ba05e8f24b98d3b38fc646
Shayan-P <shayanpardis82@gmail.com>
2023-06-25 02:22:14 -0400

    fix modularity + normalize paths

---
+++ backend/models/__init__.py
@@ -0,0 +1,3 @@
+from .image_item import ALL_IMAGE_ITEMS
+from .image_item import ImageItem, FaceItem
+from .user import User
--- backend/settings.py
+++ backend/settings.py
@@ -1,17 +1,22 @@
 import os
-from hidden_settings import *
+from .hidden_settings import SECRET_KEY
 
-BACKEND_PATH = os.path.dirname(__file__)
-PROJECT_PATH = os.path.dirname(BACKEND_PATH)
-IMAGE_SERVE_PATH = os.path.join(PROJECT_PATH, "image-serve-path")
-GALLERY_IMAGE_PATH = os.path.join(IMAGE_SERVE_PATH, "gallery-image")
-USER_IMAGE_PATH = os.path.join(IMAGE_SERVE_PATH, "user-image")
-THUMB_IMAGE_PATH = os.path.join(IMAGE_SERVE_PATH, "thumbnail")
-PICKLE_PATH = os.path.join(IMAGE_SERVE_PATH, "image-cache.pkl")
+BACKEND_PATH = os.path.normpath(os.path.dirname(__file__))
+PROJECT_PATH = os.path.normpath(os.path.dirname(BACKEND_PATH))
+IMAGE_SERVE_PATH = os.path.normpath(os.path.join(PROJECT_PATH, "image-serve-path"))
+GALLERY_IMAGE_PATH = os.path.normpath(os.path.join(IMAGE_SERVE_PATH, "gallery-image"))
+USER_IMAGE_PATH = os.path.normpath(os.path.join(IMAGE_SERVE_PATH, "user-image"))
+THUMB_IMAGE_PATH = os.path.normpath(os.path.join(IMAGE_SERVE_PATH, "thumbnail"))
+DEBUG_IMAGE_PATH = os.path.normpath(os.path.join(IMAGE_SERVE_PATH, "debug"))
+DEBUG_THUMB_IMAGE_PATH = os.path.normpath(os.path.join(IMAGE_SERVE_PATH, "thumb_debug"))
+
+PICKLE_PATH = os.path.normpath(os.path.join(IMAGE_SERVE_PATH, "image-cache.pkl"))
+TEMP_PICKLE_PATH = os.path.normpath(os.path.join(IMAGE_SERVE_PATH, "temp-image-cache.pkl"))
 
 
 def create_path_if_not_exists(path):
     if not os.path.isdir(path):
+        print(f'folder created: {path}')
         os.mkdir(path)
 
 
