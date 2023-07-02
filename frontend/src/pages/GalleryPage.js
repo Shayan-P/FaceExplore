9924c54162ff829d9d031f2e21556c37341f2df8
Shayan-P <shayanpardis82@gmail.com>
2023-07-02 15:17:47 -0400

    add Rainbow Typography to Gallery container

--- frontend/src/pages/GalleryPage.js
+++ frontend/src/pages/GalleryPage.js
@@ -1,6 +1,10 @@
 import GalleryContainer from "../components/GalleryContainer";
 import React, {useEffect, useState} from "react";
 import {similarImagesAPI} from "../api/api";
+import RainbowTypography from "../components/RainbowTypography";
+import {Container, Stack} from "@mui/material";
+import Beaver from "../components/beaver/Beaver";
+import TalkBubble from "../components/talk-bubble/TalkBubble";
 
 export default function GalleryPage() {
     const [paths, setPaths] = useState([])
@@ -8,7 +12,17 @@ export default function GalleryPage() {
     useEffect(update, [])
 
     return <>
-        <h1>Images similar to you from gallery</h1>
-        <GalleryContainer paths={paths} />
+        <>
+            <Container maxWidth="md" sx={{ height: "100vh" }}>
+                <Stack spacing={2}>
+                    <RainbowTypography variant={'h2'}
+                                       textAlign={'center'}
+                                       sx={{ display: "block", p: 2}}
+                    > Look! We found you! </RainbowTypography>
+                    <GalleryContainer paths={paths} />
+                </Stack>
+            </Container>
+            <Beaver talkBubbleElement={<TalkBubble text={'click on images to download them 😁'} />}/>
+        </>
     </>
 }
