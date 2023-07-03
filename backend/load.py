6c39cc67ae678b4e0701f26aa0bdc8a0d3e33f71
Shayan-P <shayanpardis82@gmail.com>
2023-07-03 02:51:11 -0400

    add with_face_crops=True flag to load to improve performance on runtime

--- backend/load.py
+++ backend/load.py
@@ -1,8 +1,10 @@
+from tqdm import tqdm
 from .core import load_data, get_all_gallery_images
 from .models import ImageModelWrapper
 
 
 def load():
     load_data()
-    for item in get_all_gallery_images():
-        ImageModelWrapper(item, with_thumb=True)  # just to make sure thumbs are built before we start
+    for item in tqdm(get_all_gallery_images(), 'creating ImageModelWrapper once'):
+        ImageModelWrapper(item, with_thumb=True, with_face_crops=True)
+        # just to make sure thumbs are built before we start
